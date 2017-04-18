62e137b99ce4cdb512b3dcc236ac4b3f79ca266f
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-04-18 20:17:15 +0200

    Add movement_velocity to forklift data

--- warehouse.rb
+++ warehouse.rb
@@ -29,9 +29,9 @@ WAREHOUSE_HEIGHT = 800
 class Warehouse < Chingu::Window
   def initialize
     super(WAREHOUSE_WIDTH, WAREHOUSE_HEIGHT)
-    
+
     self.factor = 1
-    
+
     switch_game_state(Room)
   end
 end
@@ -39,7 +39,7 @@ end
 class Room < Chingu::GameState
   def initialize(options = {})
     super(options)
-    
+
     self.input = {:esc => :exit}
 
     @red = Color.new(0xFFFF0000)
@@ -47,7 +47,7 @@ class Room < Chingu::GameState
     @yellow = Color.new(0xFFFFFF00)
     @white = Color.new(0xFFFFFFFF)
     @black = Color.new(0xFF000000)
-    
+
     @tokens = {}
     @forklifts = {}
     @containers = {}
@@ -57,15 +57,15 @@ class Room < Chingu::GameState
     @aware_lines = []
     @warning_lines = []
     @danger_lines = []
-    
+
     load_tokens
-    
+
     @messages = {}
     messages = @messages
-    
+
     @outbox = {created: []}
     outbox = @outbox
-    
+
     @mqtt_processor = Thread.new do
       MQTT::Client.connect(MQTT_IP) do |client|
         # From client to warehouse:
@@ -87,7 +87,7 @@ class Room < Chingu::GameState
         end
       end
     end
-    
+
     @mqtt_sender = Thread.new do
       MQTT::Client.connect(MQTT_IP) do |client|
         while true do
@@ -96,64 +96,64 @@ class Room < Chingu::GameState
               client.publish("warehouse/#{topic}", message, false)
             end
           end
-          
+
           sleep 1
         end
       end
     end
-    
+
     @mqtt_status_interval = 0
   end
-  
+
   def exit
     save_tokens
-    
+
     super
   end
-  
+
   def load_tokens
     tokens = {}
     if File.exists?('tokens.csv')
       tokens = SmarterCSV.process('tokens.csv')
     end
-    
+
     tokens.each do |record|
       @tokens[record[:name]] = record[:token]
     end
   end
-  
+
   def save_tokens
     CSV.open("tokens.csv", "w") do |csv|
       csv << ["name", "token"]
-      
+
       @forklifts.each do |token, forklift|
         @tokens[forklift.name] = token
       end
-      
+
       @tokens.each do |name, token|
         csv << [name, token]
       end
     end
   end
-  
+
   ZONES = 3
   CONTAINERS = 10
   MAX_FORKLIFTS = 10
   def setup
     @parallax = Chingu::Parallax.create(:x => 199, :y => 199, :rotation_center => :top_left)
     @parallax << { :image => "floor.png", :repeat_x => true, :repeat_y => true}
-    
+
     create_shelves
     create_delivery_zones
     create_containers
     @forklifts[:default] = create_forklift 'Default', true
     @forklifts[:default].token = :default
-    
+
     send_mqtt_warehouse_settings
-    
+
     @scoreboard = Chingu::Text.create("Loading...", :x => 10, :y => 10, :size => 30, :color => @black)
   end
-  
+
   SHELF_GAP = 60
   SHELF_CORIDOR = 200
   SHELF_WIDTH = 21
@@ -165,7 +165,7 @@ class Room < Chingu::GameState
         next if i == 0 && j == 0
         x = SHELF_GAP + ((SHELF_WIDTH + SHELF_GAP) * 2 * j)
         y = SHELF_GAP + ((SHELF_HEIGHT * 2 + SHELF_CORIDOR) * i)
-        
+
         @shelves << Shelf.create(:x => x, :y => y)
         @shelves << Shelf.create(:x => x + SHELF_WIDTH + 1, :y => y)
         @shelves << Shelf.create(:x => x, :y => y + SHELF_HEIGHT + 1)
@@ -175,11 +175,11 @@ class Room < Chingu::GameState
     end
     puts
   end
-  
+
   def create_delivery_zones
     print 'Creating delivery zones'
     ZONES.times { @zones << DeliveryZone.create(:x => rand($window.width - (300*0.25)), :y => rand($window.height - (232*0.25))) }
-    
+
     ok = false
     while !ok
       print '.'
@@ -187,14 +187,14 @@ class Room < Chingu::GameState
       [Shelf, DeliveryZone].each_collision(Shelf, DeliveryZone) do |_, zone|
         zone.x = rand($window.width - (300*0.25))
         zone.y = rand($window.height - (232*0.25))
-        
+
         ok = false
         print '.'
       end
     end
     puts
   end
-  
+
   def create_containers
     print 'Creating containers'
     CONTAINERS.times do |i|
@@ -206,7 +206,7 @@ class Room < Chingu::GameState
       c.number = i+1
       @containers[c.number] = c
     end
-    
+
     ok = false
     while !ok
       print '.'
@@ -214,15 +214,15 @@ class Room < Chingu::GameState
       [Shelf, DeliveryZone, Container, Forklift, DefaultForklift].each_collision([Shelf, DeliveryZone, Forklift, DefaultForklift], Container) do |_, container|
         container.x = rand($window.width - (50*0.5))
         container.y = rand($window.height - (40*0.5))
-        
+
         ok = false
         print '.'
       end
     end
-    
+
     puts
   end
-  
+
   def remove_forklift token
     if @forklifts[token] != nil
       forklift = @forklifts[token]
@@ -231,7 +231,7 @@ class Room < Chingu::GameState
       @forklifts.delete token
     end
   end
-  
+
   def create_forklift name, default = false
     to_be_removed = []
     @forklifts.each do |token, forklift|
@@ -241,15 +241,15 @@ class Room < Chingu::GameState
         to_be_removed << token
       end
     end
-    
+
     to_be_removed.each do |token|
       @forklifts.delete token
     end
-  
+
     print "Creating forklift \"#{name}\""
     forklift = (default ? DefaultForklift : Forklift).create(:x => rand($window.width - (117*0.5)), :y => rand($window.height - (50*0.5)), :angle => rand(360))
     forklift.name = name
-    
+
     ok = false
     while !ok
       print '.'
@@ -257,29 +257,29 @@ class Room < Chingu::GameState
       [Shelf, Container, DefaultForklift, Forklift].each_collision([Shelf, Container, DefaultForklift], Forklift) do |_, forklift|
         forklift.x = rand($window.width - (117*0.5))
         forklift.y = rand($window.height - (50*0.5))
-        
+
         ok = false
         print '.'
       end
       [Shelf, Container, Forklift, DefaultForklift].each_collision([Shelf, Container, Forklift], DefaultForklift) do |_, forklift|
         forklift.x = rand($window.width - (117*0.5))
         forklift.y = rand($window.height - (50*0.5))
-        
+
         ok = false
         print '.'
       end
     end
-    
+
     puts
-    
+
     if default
     	forklift.sensor = Sensor.create(x: forklift.x, y: forklift.y, angle: forklift.angle)
     	forklift.long_sensor = LongSensor.create(x: forklift.x, y: forklift.y, angle: forklift.angle)
     end
-    
+
     forklift
   end
-  
+
   def update_autopilot
   	@long_lines = []
   	@aware_lines = []
@@ -287,13 +287,13 @@ class Room < Chingu::GameState
   	@danger_lines = []
   	in_danger = false
   	in_warning = false
-  
+
   	player = @forklifts[:default]
   	if player
   	  Sensor.each_collision(Shelf) do |sensor, shelf|
   	  	line = {from: {x: sensor.x, y: sensor.y}, to: {x: shelf.x, y: shelf.y}}
 	  	  segment = Geometry::Segment.new_by_arrays([line[:from][:x], line[:from][:y]], [line[:to][:x], line[:from][:y]])
-	  	  
+
 	  	  if segment.length < (sensor.radius/3)*2
 					@danger_lines << line
 					in_danger = true
@@ -307,7 +307,7 @@ class Room < Chingu::GameState
 	  	Sensor.each_collision(Forklift) do |sensor, forklift|
 				line = {from: {x: sensor.x, y: sensor.y}, to: {x: forklift.x, y: forklift.y}}
 				segment = Geometry::Segment.new_by_arrays([line[:from][:x], line[:from][:y]], [line[:to][:x], line[:from][:y]])
-							
+
 				if segment.length < (sensor.radius/3)*2
 					@danger_lines << line
 					in_danger = true
@@ -322,7 +322,7 @@ class Room < Chingu::GameState
 				Sensor.each_collision(Container) do |sensor, forklift|
 					line = {from: {x: sensor.x, y: sensor.y}, to: {x: forklift.x, y: forklift.y}}
 					segment = Geometry::Segment.new_by_arrays([line[:from][:x], line[:from][:y]], [line[:to][:x], line[:from][:y]])
-								
+
 					if segment.length < (sensor.radius/3)*2
 						@danger_lines << line
 						in_danger = true
@@ -343,7 +343,7 @@ class Room < Chingu::GameState
 					@long_lines << line
 				end
 			end
-			
+
 			if player.autopilot
   			if in_danger
   				player.movement_velocity = 0.0
@@ -360,29 +360,29 @@ class Room < Chingu::GameState
 			end
    	end
   end
-  
+
   def update
     super
-    
+
     process_mqtt_commands
-    
+
     game_objects.each { |go| go.color = @white }
-    
+
     update_autopilot
-    
+
     Forklift.all.each do |forklift|
       if (forklift.x < 0 || forklift.x > $window.width) || (forklift.y < 0 || forklift.y > $window.height)
         remove_forklift forklift.token
       end
     end
-    
+
     DefaultForklift.all.each do |forklift|
       if (forklift.x < 0 || forklift.x > $window.width) || (forklift.y < 0 || forklift.y > $window.height)
         forklift.x = $window.width / 2
         forklift.y = $window.height / 2
       end
     end
-  
+
     Forklift.each_collision(DeliveryZone) do |forklift, _|
       if forklift.loaded?
         forklift.unload!
@@ -401,14 +401,14 @@ class Room < Chingu::GameState
         @containers.delete(container.number)
         container.destroy
         forklift.load!
-        
+
         create_containers if !Container.all.any?
       else
         forklift.crash!
         forklift.color = @red
       end
     end
-    
+
     DefaultForklift.each_collision(DeliveryZone) do |forklift, _|
       if forklift.loaded?
         forklift.unload!
@@ -434,88 +434,88 @@ class Room < Chingu::GameState
         forklift.color = @red
       end
     end
-    
+
     @mqtt_status_interval += 1
     if @mqtt_status_interval >= 60
       send_mqtt_status
       send_forklifts_status
       @mqtt_status_interval = 0
     end
-    
+
     @scoreboard.text = @forklifts.map {|f| "#{f[1].name}: #{f[1].score}"}.join("\n")
     @scoreboard.color = @black
-    
+
     $window.caption = "MQTT Warehouse | FPS: #{fps} | Objects: #{game_objects.size}"
   end
-  
+
   def draw
   	super
-  	
+
   	player = @forklifts[:default]
   	if player && player.debug
     	@aware_lines.each do |line|
     		draw_line line[:from][:x], line[:from][:y], Color::GREEN, line[:to][:x], line[:to][:y], Color::GREEN, 100
     	end
-    	
+
     	@warning_lines.each do |line|
     		draw_line line[:from][:x], line[:from][:y], Color::YELLOW, line[:to][:x], line[:to][:y], Color::YELLOW, 100
     	end
-    	
+
     	@danger_lines.each do |line|
     		draw_line line[:from][:x], line[:from][:y], Color::RED, line[:to][:x], line[:to][:y], Color::RED, 100
     	end
-    	
+
     	@long_lines.each do |line|
   			draw_line line[:from][:x], line[:from][:y], Color::BLUE, line[:to][:x], line[:to][:y], Color::BLUE, 100
   		end
 		end
   end
-  	
-  
+
+
   def send_forklifts_status
     @forklifts.each do |token, forklift|
       next if !forklift.dirty
-    
+
       payload = {status: forklift.data, time: Time.now}
       forklift.dirty = false
-      
+
       send_mqtt_command "#{token}/status", JSON.generate(payload)
     end
   end
-  
+
   def send_mqtt_status
     payload = {forklifts: {}, containers: [], time: Time.now}
-    
+
     @forklifts.each do |_, forklift|
       payload[:forklifts][forklift.name] = forklift.data
     end
-    
+
     @containers.each do |_, container|
       payload[:containers] << container.data
     end
-  
+
     send_mqtt_command 'status', JSON.generate(payload)
   end
-  
+
   def process_mqtt_commands
     @messages.each do |name, queue|
       if queue.any?
         message = @messages[name].shift
         #puts "Use #{name}: #{message}"
-        
+
         # name=Robot,key=<KEY>
         if name == 'create'
           if @forklifts.size < MAX_FORKLIFTS
             data = parse_message_payload message
             topic = "created/#{data[:key]}"
-            
+
             if data[:name] != nil
               token = @tokens[data[:name]] || Digest::SHA1.hexdigest(data[:name] + Time.now.to_s)
-              
+
               @tokens[data[:name]] = token
               @forklifts[token] = create_forklift data[:name]
               @forklifts[token].token = token
-              
+
               send_mqtt_command topic, "name=#{data[:name]},token=#{token}"
             else
               puts "Received invalid create message \"#{message}\""
@@ -525,7 +525,7 @@ class Room < Chingu::GameState
           end
         elsif name.include? '/remove'
           token = parse_token name
-          
+
           if @forklifts[token] != nil
             puts "Removing forklift \"#{@forklifts[token].name}\"..."
             @forklifts[token].destroy
@@ -535,7 +535,7 @@ class Room < Chingu::GameState
           end
         elsif name.include? '/control'
           token = parse_token name
-          
+
           if @forklifts[token] != nil
             @forklifts[token].control message
           else
@@ -545,14 +545,14 @@ class Room < Chingu::GameState
       end
     end
   end
-  
+
   def parse_token topic
     topic.split('/')[0]
   end
-  
+
   def send_mqtt_warehouse_settings
     payload = {dimensions: {}, positions: {}, time: Time.now}
-    
+
     payload[:dimensions][:arena] = {h: WAREHOUSE_HEIGHT, w: WAREHOUSE_WIDTH}
     payload[:dimensions][:forklift] = {h: 25, w: 59, r: 30}
     payload[:dimensions][:container] = {h: 20, w: 25, r: 16}
@@ -560,30 +560,30 @@ class Room < Chingu::GameState
     payload[:dimensions][:shelve] = {h: 105, w: 21}
     payload[:positions][:shelves] = []
     payload[:positions][:zones] = []
-    
+
     @shelves.each do |shelf|
       payload[:positions][:shelves] << shelf.data
     end
-    
+
     @zones.each do |zone|
       payload[:positions][:zones] << zone.data
     end
-    
+
     MQTT::Client.connect(MQTT_IP) do |client|
       client.publish("warehouse/settings", JSON.generate(payload), true)
     end
   end
-  
+
   def send_mqtt_command topic, message
     @outbox[topic] ||= []
     @outbox[topic] << message
   end
-  
+
   def parse_message_payload message
     raw_data = message.gsub(/\s/, '').split(',').map{|x|a = x.split('=');{a[0].to_sym => a[1]}}
     data = {}
     raw_data.each { |d| data.merge!(d) }
-    
+
     data
   end
 end
@@ -593,14 +593,14 @@ class Shelf < GameObject
   traits :collision_detection
 
   attr_reader :data
-  
+
   def setup
     @image = Image["shelf.png"]
     self.factor = 0.5
-    
+
     cache_bounding_box
   end
-  
+
   def data
     {x: self.x, y: self.y}
   end
@@ -612,7 +612,7 @@ end
 class Container < GameObject
   trait :bounding_circle, :debug => true
   traits :collision_detection
-  
+
   attr_reader :data
   attr_accessor :number
 
@@ -622,7 +622,7 @@ class Container < GameObject
 
     cache_bounding_circle
   end
-  
+
   def data
     {x: x, y: y, n: number}
   end
@@ -640,18 +640,18 @@ class DeliveryZone < GameObject
   traits :collision_detection
 
   attr_reader :data
-  
+
   def setup
     @image = Image["stripes.png"]
     self.factor = 0.25
 
     cache_bounding_box
   end
-  
+
   def data
     {x: x, y: y}
   end
-  
+
   def w; 75; end
   def h; 58; end
 end
@@ -667,10 +667,10 @@ MAX_ANGULAR_ACCELERATION   = 1.00001    # -1.0 - 1.0
 class Forklift < GameObject
   trait :bounding_circle, :debug => true
   traits :velocity, :collision_detection, :rotation, :direction
-  
+
   attr_accessor :name, :dirty, :token
   attr_reader :score, :data
-  
+
   def setup
     @image = Image["forklift.png"]
     @loaded = false
@@ -689,21 +689,21 @@ class Forklift < GameObject
 
     self.factor = 0.5
     self.rotation_center = :center
-    
+
     cache_bounding_circle
   end
-  
+
   def control command
     # r=0.5,m=0.5
     puts "Forklift #{self.name}: Received command \"#{command}\""
     commands = parse_command_data command
-    
+
     if commands[:r] != nil
       @dirty = true
       self.angular_acceleration = [[commands[:r].to_f, -self.max_angular_acceleration].max, self.max_angular_acceleration].min
       puts "Forklift #{self.name}: Setting angular acceleration to #{self.angular_acceleration}"
     end
-    
+
     if commands[:m] != nil
       @dirty = true
       delta = [[commands[:m].to_f, -MAX_MOVE_ACCELERATION].max, MAX_MOVE_ACCELERATION].min
@@ -712,72 +712,72 @@ class Forklift < GameObject
       puts "Forklift #{self.name}: Adjusting velocity by #{delta}"
     end
   end
-   
+
   def data
-    {name: self.name, x: self.x, y: self.y, angle: self.angle, score: self.score, loaded: loaded?}
+    {name: self.name, x: self.x, y: self.y, angle: self.angle, speed: self.movement_velocity, score: self.score, loaded: loaded?}
   end
-  
+
   def w; 59; end
   def h; 25; end
-  
+
   def parse_command_data command
     raw_data = command.split(',').map{|x|a = x.split('=');{a[0].to_sym => a[1]}}
     data = {}
     raw_data.each { |d| data.merge!(d) }
-    
+
     data
   end
-  
+
   def radius
     30
   end
-  
+
   def delimeter
   	radius * 2
   end
-  
+
   def crash!
     @dirty = true
     @score -= 1
   end
-  
+
   def add_delivery!
     @dirty = true
     @score += 10
   end
-  
+
   def load!
     @dirty = true
     @image = Image["forklift_loaded.png"]
     @loaded = true
   end
-  
+
   def unload!
     @dirty = true
     @image = Image["forklift.png"]
     @loaded = false
   end
-  
+
   def loaded?
     @loaded
   end
-  
+
   def update
     # dampening
     apply_dampening
-  
+
     # screen edge bouncing
     #self.velocity_x = -self.velocity_x if @x < 0 || @x > $window.width
     #self.velocity_y = -self.velocity_y if @y < 0 || @y > $window.height
   end
-  
+
   def apply_dampening
     self.velocity_x += (self.velocity_x > 0) ? -MOVE_DAMPENING : MOVE_DAMPENING if self.velocity_x != 0 && self.velocity_x.abs > MOVE_DAMPENING
     self.velocity_x = 0 if self.velocity_x.abs <= MOVE_DAMPENING
     self.velocity_y += (self.velocity_y > 0) ? -MOVE_DAMPENING : MOVE_DAMPENING if self.velocity_y != 0 && self.velocity_y.abs > MOVE_DAMPENING
     self.velocity_y = 0 if self.velocity_y.abs <= MOVE_DAMPENING
   end
-  
+
   def self.inside_viewport
     all.select { |forklift| forklift.game_state.viewport.inside?(forklift) }
   end
@@ -813,28 +813,28 @@ class DefaultForklift < Forklift
 
   def setup
     super
-  
+
     @autopilot = false
     @debug = false
     self.input = [:holding_left, :holding_right, :holding_down, :holding_up, :s, :r, :a, :d]  # NOTE: giving input an Array, not a Hash
   end
-  
+
   def update
   	super
-  	
+
   	if self.sensor
 	  	self.sensor.x = self.x
 	  	self.sensor.y = self.y
 	  	self.sensor.angle = self.angle
   	end
-  	
+
   	if self.long_sensor
 		  self.long_sensor.x = self.x
 		  self.long_sensor.y = self.y
 		  self.long_sensor.angle = self.angle
 		end
   end
-  
+
   # state_machine :state, initial: :parked do
   # end
 
@@ -849,7 +849,7 @@ class DefaultForklift < Forklift
     @autopilot = !@autopilot
     self.angular_acceleration = 0.0 if !@autopilot
   end
-  
+
 end
 
 Warehouse.new.show
